56c693327fdec373e60ed5abe0eb28f2775a2c1f
Quinten <tintin10q@hotmail.com>
2021-05-12 22:50:05 +0200

    Better formatting 🎆

--- utils/get_env_var.py
+++ utils/get_env_var.py
@@ -1,10 +1,11 @@
 """Loads and returns the key from a .env file"""
 
-from dotenv import load_dotenv   #for python-dotenv method
-load_dotenv()                    #for python-dotenv method
+from dotenv import load_dotenv
+
+load_dotenv()
 
 import os
 
+
 def get_env_var(var_name: str) -> str:
     return os.environ.get(var_name)
-
