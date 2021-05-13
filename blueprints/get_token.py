78ebad5191fa8e09ac2e8bf627bcf9b1dd9a11ec
Quinten <tintin10q@hotmail.com>
2021-05-13 02:47:51 +0200

    Added header validation to get_token.py 🎈

--- blueprints/get_token.py
+++ blueprints/get_token.py
@@ -1,13 +1,17 @@
 from flask import Blueprint, render_template, request, redirect, flash
+import jwt
+import validation
 
 get_token_blueprint = Blueprint('get_token', __name__)
 
 
-@get_token_blueprint.route("/get_token", methods=["GET"])
-def get_token_get():
-    return render_template("get_token.html")
-
-
 @get_token_blueprint.route("/get_token", methods=["POST"])
+@validation.has_header("data")
+@validation.header_is_json("data")
+@validation.header_has_key("data", "username")
+@validation.header_has_key("data", "password")
 def get_token_post():
-    return render_template("get_token.html")
+    return "NIY"
+
+
+# https://docs.aiohttp.org/en/stable/ https://pypi.org/project/jwt4auth/ https://pypi.org/project/jwt4auth/https://pypi.org/project/jwt4auth/https://pypi.org/project/jwt4auth/
